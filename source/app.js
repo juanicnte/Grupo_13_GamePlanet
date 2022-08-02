4dfd48f09d776e52d8382395d7028c7f72a95044
7amares7 <7a.mares7@gmail.com>
2022-08-02 14:34:22 -0500

    Configurando el app.js

--- source/app.js
+++ source/app.js
@@ -1,24 +1,24 @@
 const express = require('express');
-const {resolve} = require('path');
+const path = require ('path');
 const app = express();
-const public = path.resolve(__dirname, '../public');
-app.use(express.static(public)) 
+const publicPath = path.resolve(__dirname, '../public');
+app.use(express.static(publicPath)) 
 app.listen(process.env.PORT || 3030, () => {
     console.log('servidor corriendo...');
 });
 
 app.get ('/', (req, res) => {
-    res.sendFile(resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
 app.get("/register", function (req, res) {
-    res.sendFile(resolve(__dirname, './views/register.html'));
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
 app.get("/login", function (req, res) {
-    res.sendFile(resolve(__dirname, './views/login.html'));
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
 app.post("/", function (req, res) {
-    res.sendFile(resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
