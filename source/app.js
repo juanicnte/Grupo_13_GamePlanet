6472aa535cf79f6004073783eca784f1454c3ef7
7amares7 <7a.mares7@gmail.com>
2022-08-01 23:20:20 -0500

    Incorporando estilos register y login

--- source/app.js
+++ source/app.js
@@ -1,24 +1,24 @@
 const express = require('express');
-const path = require ('path');
+const {resolve} = require('path');
 const app = express();
-const publicPath = path.resolve(__dirname, '../public');
-app.use(express.static(publicPath)) 
+const public = path.resolve(__dirname, '../public');
+app.use(express.static(public)) 
 app.listen(process.env.PORT || 3030, () => {
     console.log('servidor corriendo...');
 });
 
 app.get ('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(resolve(__dirname, './views/home.html'));
 });
 
 app.get("/register", function (req, res) {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
+    res.sendFile(resolve(__dirname, './views/register.html'));
 });
 
 app.get("/login", function (req, res) {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
+    res.sendFile(resolve(__dirname, './views/login.html'));
 });
 
 app.post("/", function (req, res) {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(resolve(__dirname, './views/home.html'));
 });
