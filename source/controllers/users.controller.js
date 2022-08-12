422b6815f728755e8108d444d642c619d66a6160
7amares7 <7a.mares7@gmail.com>
2022-08-12 00:53:56 -0500

    Merge Sprint 3

--- source/controllers/users.controller.js
+++ source/controllers/users.controller.js
@@ -1,3 +1,11 @@
-const controlador = {};
+const { write } = require('fs');
+const path = require('path');
+const {generate} = require('../models/users.model')
+const controlador = {
+    create: (req, res) => {
+        let nuevo = generate(req.body);
+        return res.send('Creado')
+    }
+};
 
 module.exports = controlador;
