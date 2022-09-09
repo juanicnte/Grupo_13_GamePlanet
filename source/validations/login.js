6c5ef278d01383863758bd250a11a3bef8271ab1
Franco Seccaspina <francosec2002@hotmail.com>
2022-09-09 17:03:35 -0300

    subiendo validaciones

---
+++ source/validations/login.js
@@ -0,0 +1,44 @@
+const { body } = require('express-validator')
+
+const all = require('../models/users.model')
+
+const { compareSync } = require('bcryptjs')
+
+let email = body('email').notEmpty().withMessage('Email no puede quedar vacio').bail().isEmail().withMessage('Email no es valido').custom((value,{req})=>{
+
+    let users = all()
+
+    let listEmails = users.map(user => user.users.mail)
+
+    if(!listEmails.indexOf(value) == -1){
+
+        throw new Error('Usuario no encontrado')
+    }
+
+    return true;
+
+})
+
+let password = body('password').notEmpty().withMessage('Contrasela es invalida').bail().isLength({min:4,max:30}).withMessage('Minimo de 4 caracteres').custom((value,{req})=>{
+
+    let users = all()
+
+    let result = users.find(user =>user.email == req.body.email)
+
+    if(!result){
+
+        throw new Error('Contraseña es invalida')
+    }
+
+    if(!compareSync(value,result.password)){
+
+        throw new Error('La contraseña no coincide')
+    }
+
+    return true
+
+})
+
+let validaciones =[email,password]
+
+module.exports= validaciones
