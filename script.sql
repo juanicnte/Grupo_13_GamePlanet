8f14b64cd4e9a4867cf829d27d513be68692bb8c
7amares7 <7a.mares7@gmail.com>
2022-09-27 19:10:34 -0500

    Script datos para la base de datos

---
+++ script.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories (name) VALUES ('Nintendo');
+INSERT INTO categories (name) VALUES ('PC');
+INSERT INTO categories (name) VALUES ('PlayStation');
+INSERT INTO categories (name) VALUES ('Xbox');
