987ff61aa778b1a600a6abff038217e3bed6de54
7amares7 <7a.mares7@gmail.com>
2022-10-24 19:00:34 -0500

    Recupero las validaciones del login perdidas por el merge

--- backend/source/validations/login.js
+++ backend/source/validations/login.js
@@ -1,54 +1,13 @@
-<<<<<<< HEAD
-const { body } = require('express-validator')
-
-const { compareSync } = require('bcryptjs')
-=======
 const { body, check } = require('express-validator')
-const { bcryptjs } = require('bcryptjs')
->>>>>>> bd016d0f06e4822725a7f5676f8216bbb91c8abf
+const { compareSync } = require('bcryptjs')
 
 const db = require('../database/models/index');
 const { nextTick } = require('process');
 console.log("he ingresado al validador back")
 
-
-
-let email = body('email').notEmpty().withMessage('E-Mail no puede quedar vacíoaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa').bail().
-custom(function(user){
-    db.user.findOne({where:
-    {
-        email: req.body.email
-    }}).then(function(user){
-        if(!user){
-            throw new Error('used email')
-        }else{
-            true
-        }
-    })
-}).withMessage('Email no registrado')
+let email = body('email').notEmpty().withMessage('E-Mail no puede quedar vacío').bail().isEmail().withMessage('Email no valido').bail()
 let password = body('password').notEmpty().withMessage('Por favor, ingrese su contraseña').bail()
 
-<<<<<<< HEAD
-   
-
-
-    
-    
-
-
-let validaciones = [email,password]
-
-module.exports = validaciones;
-          
-
-            
-            
-         
-      
-   
-
-
-=======
 module.exports = [
     
     email, password,
@@ -60,22 +19,30 @@ module.exports = [
         }).then(user => {
           if (!user) {
             return Promise.reject('E-mail no existe');
-          }/*
-          if(!bcryptjs.compareSync(user.password, value)){
-            return Promise.reject('La clave no coincide');
-          }*/
+          }
         });
-      })/*,
-      check('password').custom(value => {
-          return db.user.findOne({
-              where:{
-                password: bcryptjs.hashSync(value,10)
-              }
-          }).then(user => {
-            if (!user) {
-              return Promise.reject('La clave no coincide');
-            }
-          });
-        })*/
+      })
+
+    /*
+    body('password')
+        .custom((async function(req,res) {  
+            usuario = db.user.findOne({
+                where:{
+                    email: req.body.email
+                }
+            })
+            console.log(usuario)
+            if(usuario) {
+                res.status(400).json({
+                    message: 'This login is already taken. Try another.'
+                })}
+            ((user) => {
+ 
+                if(!compareSync(req.body.password, user.password)){
+                     console.log('CONTRASEÑA INCORRECTAAAAAAAAAAAAAA');
+                    return Promise.reject("Email o contraseña incorrectos")
+                }
+            })
+ 
+        }))*/
 ]  
->>>>>>> bd016d0f06e4822725a7f5676f8216bbb91c8abf
