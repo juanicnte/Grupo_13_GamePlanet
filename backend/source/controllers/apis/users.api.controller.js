6ca80082d9d7c6fdd042550aa9efda77df29a884
juanicnte <juanmacen547@gmail.com>
2022-11-11 22:06:07 -0300

    controlador api usuarios

---
+++ backend/source/controllers/apis/users.api.controller.js
@@ -0,0 +1,26 @@
+const db = require('../database/models/index');
+const { body, validationResult } = require('express-validator')
+const bcryptjs = require('bcryptjs')
+
+const controlador = {
+    index: function(req, res){
+        let users = db.user.findAll().then(function(users){
+            return res.send('users/adminUsers',{users:users})
+        })
+    },
+    show: function(req, res){
+        db.user.findOne({
+            where:{
+                id:req.params.id
+            }
+        }).then(function(user){
+            if(user){
+                return res.send('users/userDetail',{ user:user });
+            }
+            res.redirect('/')
+        })
+        /*let product = products.filter(product => product.sku == req.params.id);*/
+    }
+};
+
+module.exports = controlador;
