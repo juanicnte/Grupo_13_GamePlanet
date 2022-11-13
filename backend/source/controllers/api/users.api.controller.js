18163b1a46220c2f52d156afd3a27acf1a218553
Lucas-Ojeda <lucasojeda00@hotmail.com>
2022-11-13 00:11:43 -0300

    api de users controller

--- backend/source/controllers/api/users.api.controller.js
+++ backend/source/controllers/api/users.api.controller.js
@@ -1,26 +1,21 @@
-const db = require('../../database/models/index');
-const { body, validationResult } = require('express-validator')
-const bcryptjs = require('bcryptjs')
+const db = require('../../database/models');
+const User = db.user;
 
-const controlador = {
-    index: function(req, res){
-        let users = db.user.findAll().then(function(users){
-            return res.send('users/adminUsers',{users:users})
-        })
+const usersAPIController =  {
+    'list': (req, res) => {
+        User.findAll()
+            .then(users => {
+                let respuesta = {
+                    meta: {
+                        status: 200,
+                        total: users.length,
+                        url: 'api/users'
+                    },
+                    data: users
+                };
+                res.json(respuesta);
+            });
     },
-    show: function(req, res){
-        db.user.findOne({
-            where:{
-                id:req.params.id
-            }
-        }).then(function(user){
-            if(user){
-                return res.send('users/userDetail',{ user:user });
-            }
-            res.redirect('/')
-        })
-        /*let product = products.filter(product => product.sku == req.params.id);*/
-    }
-};
 
-module.exports = controlador;
+};
+module.exports = usersAPIController;
