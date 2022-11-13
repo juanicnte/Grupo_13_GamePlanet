c3c29780012e2fcb8858ab96c2e80bf8442bed49
juanicnte <juanmacen547@gmail.com>
2022-11-12 21:54:06 -0300

    rutas de la api de categorias

--- backend/source/routes/api/categories.api.routes.js
+++ backend/source/routes/api/categories.api.routes.js
@@ -1,34 +1,10 @@
 const express = require('express');
 
-const usersController = require('../controllers/api/users.api.controller');
-
 const route = express.Router();
 
-const {resolve, extname} = require('path');
+const categoriesController = require('../../controllers/api/categories.api.controller');
 
-const { existsSync, mkdirSync } = require('fs');
-
-
-const multer = require('multer');
-const upload = multer({storage:multer.diskStorage({destination, filename})});
-
-const registerValidator = require('../validations/register')
-
-const loginValidator = require('../validations/login')
-
-const isLogged = require('../middlewares/isLogged')
-const isAdmin = require('../middlewares/isAdmin')
-
-
-
-route.get('/register', usersController.create)
-
-route.get('/login', usersController.login)
-
-route.get('/users/detail/:id', isLogged, usersController.show)
-
-route.put('/users/:id', isLogged, usersController.show)
-
-route.get('/users', isLogged, isAdmin, usersController.index)
+//route.get('/api/categories', isLogged, isAdmin, categoriesController.list)
+route.get('/api/categories', categoriesController.list)
 
 module.exports = route;
