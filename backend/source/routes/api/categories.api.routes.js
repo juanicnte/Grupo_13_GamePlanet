9f411fc79653929f8fd9c6499baeeaf68e7e52f7
Lucas-Ojeda <lucasojeda00@hotmail.com>
2022-11-13 00:40:42 -0300

    ruta de categoria del producto

--- backend/source/routes/api/categories.api.routes.js
+++ backend/source/routes/api/categories.api.routes.js
@@ -5,6 +5,8 @@ const route = express.Router();
 const categoriesController = require('../../controllers/api/categories.api.controller');
 
 //route.get('/api/categories', isLogged, isAdmin, categoriesController.list)
-route.get('/api/categories', categoriesController.list)
+route.get('/api/categories', categoriesController.list);
+route.get('/api/categories/products/:id', categoriesController.categoryProducts);
+
 
 module.exports = route;
