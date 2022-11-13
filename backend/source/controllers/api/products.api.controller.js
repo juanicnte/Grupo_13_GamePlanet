0566ccb20640e1b51350615a33733d338e03782c
Lucas-Ojeda <lucasojeda00@hotmail.com>
2022-11-12 23:52:08 -0300

    api de productos en el controller

--- backend/source/controllers/api/products.api.controller.js
+++ backend/source/controllers/api/products.api.controller.js
@@ -1,18 +1,21 @@
-const db = require('../../database/models/index');
+const db = require('../../database/models');
+const Product = db.product;
 
-const controlador = {
-    index: (req, res) => {
-        const products = db.product.findAll()
-        const success = data => res.send('home',{ products: data})
-        const error = error => res.send(error)
-        products.then(success).catch(error)
+const productsAPIController =  {
+    'list': (req, res) => {
+        Product.findAll()
+            .then(products => {
+                let respuesta = {
+                    meta: {
+                        status: 200,
+                        total: products.length,
+                        url: 'api/products'
+                    },
+                    data: products
+                };
+                res.json(respuesta);
+            });
     },
-    show: function(req, res){
-        const products = db.product.findByPk(req.params.id)        
-        const success = data => res.send('productDetail',{ product: data})
-        const error = error => res.send(error)
-        products.then(success).catch(error)
-    }
-}
 
-module.exports = controlador;
+};
+module.exports = productsAPIController;
