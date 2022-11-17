abe77353128ffbe8dac4138444fe278ee8dc8be2
juanicnte <juanmacen547@gmail.com>
2022-11-16 23:16:57 -0300

    añadido funcionalidad filtrado de productos

---
+++ backend/public/js/search-filter.js
@@ -0,0 +1,18 @@
+
+  document.querySelector('#search-input').addEventListener('input', filterList)
+
+function filterList(){
+  const searchInput = document.querySelector('#search-input')
+  const filter = searchInput.value.toLowerCase()
+  const listItems = document.querySelectorAll('.list-group-item')
+  
+  listItems.forEach((item) => {
+    let text = item.textContent;
+    if(text.toLowerCase().includes(filter.toLowerCase())){
+      item.style.display = '';
+    }
+    else{
+      item.style.display = 'none';
+    }
+  })
+}
