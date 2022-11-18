42cc7ebb369f15a7aa5d90b5058c37068346ccc8
Marcia Dienny Montoya Vicente <MMontoyaV@leonisa.com>
2022-11-17 21:13:19 -0500

    Filtros categorías y limpiar filtros (ver todo)

--- backend/public/js/category-filter.js
+++ backend/public/js/category-filter.js
@@ -1,94 +1,33 @@
 
 window.addEventListener('load', function () {
 
-  document.querySelector("#category1").addEventListener('click', e => {
-    console.log('lo intenta1')
-    e.preventDefault()
-    const category1 = document.querySelector('#category1')
+  function filterCategories(name, idCategoria) {
+
+    const category3 = document.querySelector(name)
     const listItems = document.querySelectorAll('.list-group-item')
     listItems.forEach((item) => {
-      if (item.categoryId == 1) {
+      const category3 = item.querySelector('#categoryId')
+
+      if (category3.value == idCategoria) {
         item.style.display = '';
       }
       else {
         item.style.display = 'none';
       }
     })    
-   })
+   }
+
+  document.querySelector("#category1").addEventListener('click',  e => {
+    filterCategories("#category1", 1) })
 
   
    document.querySelector("#category2").addEventListener('click', e => {
-    console.log('lo intenta2')
-    e.preventDefault()
-
-    const category2 = document.querySelector('#category2')
-    const listItems = document.querySelectorAll('.list-group-item')
-    listItems.forEach((item) => {
-      if (item.categoryId == 2) {
-        item.style.display = '';
-      }
-      else {
-        item.style.display = 'none';
-      }
-    })    
-   })
+    filterCategories("#category2", 2) })
 
    document.querySelector("#category3").addEventListener('click', e => {
-    console.log('lo intenta3')
-    e.preventDefault()
-
-    const category3 = document.querySelector('#category3')
-    const listItems = document.querySelectorAll('.list-group-item')
-    listItems.forEach((item) => {
-      console.log(item)
-
-      if (item.categoryId == 3) {
-        item.style.display = '';
-      }
-      else {
-        console.log('entro 3')
-        item.style.display = 'none';
-      }
-    })    
-   })
-
+    filterCategories("#category3", 3) })
+   
    document.querySelector("#category4").addEventListener('click', e => {
-    console.log('lo intenta4')
-    e.preventDefault()
-
-    const category4 = document.querySelector('#category4')
-    const listItems = document.querySelectorAll('.list-group-item')
-    listItems.forEach((item) => {
-      if (item.categoryId == 4) {
-        item.style.display = '';
-      }
-      else {
-        item.style.display = 'none';
-      }
-    }) 
-   })
+    filterCategories("#category4", 4) })
     
 })
-
-  /*document.querySelector('#category2').addEventListener('click', filterCategories('#category2', 2))
-  document.querySelector('#category3').addEventListener('click', filterCategories('#category3', 3))
-  document.querySelector('#category4').addEventListener('click', filterCategories('#category4', 4))
-  function filterCategories(name, cat) {
-    console.log('lo intenta3')
-    console.log(name)
-
-    const category1 = document.querySelector(name)
-    const listItems = document.querySelectorAll('.list-group-item')
-
-    listItems.forEach((item) => {
-      let text = item.textContent;
-      if (item.categoryId == cat) {
-        item.style.display = '';
-      }
-      else {
-        item.style.display = 'none';
-      }
-    })
-  }
-})
-*/
--- backend/public/js/search-filter.js
+++ backend/public/js/search-filter.js
@@ -1,18 +1,26 @@
 
+window.addEventListener('load', function () {
   document.querySelector('#search-input').addEventListener('input', filterList)
 
-function filterList(){
-  const searchInput = document.querySelector('#search-input')
-  const filter = searchInput.value.toLowerCase()
-  const listItems = document.querySelectorAll('.list-group-item')
-  
-  listItems.forEach((item) => {
-    let text = item.textContent;
-    if(text.toLowerCase().includes(filter.toLowerCase())){
+  function filterList() {
+    const searchInput = document.querySelector('#search-input')
+    const filter = searchInput.value.toLowerCase()
+    const listItems = document.querySelectorAll('.list-group-item')
+
+    listItems.forEach((item) => {
+      let text = item.textContent;
+      if (text.toLowerCase().includes(filter.toLowerCase())) {
+        item.style.display = '';
+      }
+      else {
+        item.style.display = 'none';
+      }
+    })
+  }
+  document.querySelector('#clearFilter').addEventListener('click', e => {
+    const listItems = document.querySelectorAll('.list-group-item')
+    listItems.forEach((item) => {
       item.style.display = '';
-    }
-    else{
-      item.style.display = 'none';
-    }
+    })
   })
-}
+})
